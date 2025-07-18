febb53329f1eefd947e525f8802e8fd2f6891a68
Ezz Eldin Ahmed <ezzeldinahmad96@gmail.com>
2025-07-18 14:23:57 +0300

    Update settings.py

--- ezzportfolio/settings.py
+++ ezzportfolio/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = "ezzportfolio.wsgi.app"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'D0pRztOHQSnRniTS',
+        'HOST': 'https://gefqshdrgozkxdiuligl.supabase.co',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
