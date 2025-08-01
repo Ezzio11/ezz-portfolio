27657c834f19e0453151b7fe6d4bedd5378fa7ad
Ezz Eldin Ahmed <ezzeldinahmad96@gmail.com>
2025-08-01 16:39:49 +0300

    Update urls.py

--- ezzportfolio/urls.py
+++ ezzportfolio/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('download/resume/', views.resume_dl, name='resume_dl'),
@@ -34,5 +32,6 @@ urlpatterns = [
     path('mstag/the-decline-of-the-polymath/', views.decline_of_polymath, name='decline_of_polymath'),
     path('mstag/<slug:slug>/', views.article_detail, name='article_detail'),
     path("api/polymaths/", views.polymaths_api, name="polymaths_api"),
-    path('chatbot/', views.chatbot_html, name='chatbot'),
+    path('chatbot_xane/', views.chatbot_html, name='chatbot'),
+    path('chatbot/', views.chatbot_api, name='chatbot_api'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
